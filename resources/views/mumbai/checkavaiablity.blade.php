7d3ac8202722262ba10428b1265aa59f2b836c79
mohammedsahilgour <sahilgour5675@gmail.com>
2025-01-28 21:48:38 +0530

    make popup box in check avaiablity section

--- resources/views/mumbai/checkavaiablity.blade.php
+++ resources/views/mumbai/checkavaiablity.blade.php
@@ -9,12 +9,12 @@
             <img src="{{ $hotel['hotel_image'] }}" alt="Overview Image" class="hotel-overview-image">
         </div>
         <div class="overview-content">
-            <h1 class="overview-title">Over View </h1><br>
+            <h1 class="overview-title">Overview</h1><br>
             <p class="overview-description">
-                Welcome to {{ $hotel['hote_name'] }} 
+                Welcome to {{ $hotel['hote_name'] }}
             </p>
             <p class="overview-description">
-                {{ $hotel['overview'] }} 
+                {{ $hotel['overview'] }}
             </p>
             <ul class="overview-features">
                 <li>✓ Spacious and elegant rooms</li>
@@ -22,6 +22,16 @@
                 <li>✓ 24/7 room service</li>
                 <li>✓ Easy access to popular attractions</li>
             </ul>
+        </div>
+    </div>
+
+    <!-- Improved Hotel Name and Price Section -->
+    <div class="center-box mt-5">
+        <h2 class="hotel-name"> Hotel Name  <h4>{{ $hotel['hote_name'] }}</h4></h2>
+        <p class="hotel-price"> price₹  {{ $hotel['hotel_price'] }}</p>
+        <div class="both-btn">
+        <a href="#" class="book-now-link">Book Now</a>
+        <button type="button" class="btn btn-danger">Cancel</button>
         </div>
     </div>
 
@@ -33,8 +43,7 @@
                     <th>Hotel Image</th>
                     <th>Hotel Name</th>
                     <th>Price (per night)</th>
-                    <th>Avaiablity</th>
-                    <th>Selectd  Room</th>
+                    <th>Availability</th>
                 </tr>
             </thead>
             <tbody>
@@ -49,21 +58,15 @@
                             Select
                         </button>
                     </td>
-                    <td > <b style='color:red'>Room Not  Selected </b></td>
-
                 </tr>
             </tbody>
         </table>
     </div>
-</div><br><br><br><br><br><br><br><br><br>
+</div><br><br><br><br><br><br><br><br><br><br><br>
 
 <script>
 $(document).ready(function () {
     console.log("Hotel availability page ready!");
-
-   
-   
 });
 </script>
-
 @endsection
